54b98d3d22707e5e644e68b829949a9fc9083389
daniela_rueda <drueda@makingsense.com>
2016-09-14 14:41:45 -0300

    se crea las funciones necesarias para el funcionamiento de los widgets

--- wp-content/themes/twentysixteen/functions.php
+++ wp-content/themes/twentysixteen/functions.php
@@ -153,7 +153,7 @@ add_action( 'after_setup_theme', 'twentysixteen_content_width', 0 );
  */
 function twentysixteen_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'twentysixteen' ),
+		'name'          => __( 'Sidebar (blog)', 'twentysixteen' ),
 		'id'            => 'sidebar-1',
 		'description'   => __( 'Add widgets here to appear in your sidebar.', 'twentysixteen' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
@@ -175,6 +175,24 @@ function twentysixteen_widgets_init() {
 	register_sidebar( array(
 		'name'          => __( 'Content Bottom 2', 'twentysixteen' ),
 		'id'            => 'sidebar-3',
+		'description'   => __( 'Appears at the bottom of the content on posts and pages.', 'twentysixteen' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+        register_sidebar( array(
+		'name'          => __( 'Navigation-homepage', 'twentysixteen' ),
+		'id'            => 'navigation',
+		'description'   => __( 'Appears at the bottom of the content on posts and pages.', 'twentysixteen' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+        register_sidebar( array(
+		'name'          => __( 'Blocks-homepage', 'twentysixteen' ),
+		'id'            => 'block-home',
 		'description'   => __( 'Appears at the bottom of the content on posts and pages.', 'twentysixteen' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',

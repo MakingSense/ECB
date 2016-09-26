25e0cb85607780de2c03abcd7935e7a05ca83109
daniela_rueda <drueda@makingsense.com>
2016-09-26 17:34:53 -0300

    Se embebio el formulario con la cuenta newsletter Constant Contact

--- wp-content/themes/twentysixteen/footer.php
+++ wp-content/themes/twentysixteen/footer.php
@@ -13,27 +13,33 @@
 
 			</div> <!-- .content -->
 	 </main><!-- .section--home -->
- <!-- Begin MailChimp Signup Form -->
+
 	 <footer class="component--footer">
 	   <h2>Stay in Touch</h2>
 	   <p>Sign up to receive our monthly newsletter and the latest Ecocity news</p>
-
-	   <div class="suscribe-container" id="mc_embed_signup_scroll">
-               <form action="//twitter.us14.list-manage.com/subscribe/post?u=b4abeae17819a0b0878b66c14&amp;id=dc17bbb6c9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                    <label>
-                        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email">
-                    </label>
-                      <button type="submit" class="suscribe-button">Suscribe</button>
-                      <div id="mce-responses" class="clear">
-                            <div class="response" name="subscribe" id="mc-embedded-subscribe" id="mce-error-response" style="display:none"></div>
-                            <div class="response" id="mce-success-response" style="display:none"></div>
-                      </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <!--Begin CTCT Sign-Up Form-->
+            <!-- EFD 1.0.0 [Mon Sep 26 08:32:50 EDT 2016] -->
+               <div class="suscribe-container ctct-embed-signup" >
+                   <span id="success_message" style="display:none;">
+                       <div style="text-align:center;">Thanks for signing up!</div>
+                   </span>
+                   <form data-id="embedded_signup:form" class="ctct-custom-form Form" name="embedded_signup" method="POST" action="https://visitor2.constantcontact.com/api/signup">
                       
-                    <span class="submit-message">Submit Message</span>
-               </form>
-	   </div>
-            <!--End mc_embed_signup-->
-           
+                       <!-- The following code must be included to ensure your sign-up form works properly. -->
+                       <input data-id="ca:input" type="hidden" name="ca" value="f99de5de-dc09-47d1-bc25-5f778694a5d5">
+                       <input data-id="list:input" type="hidden" name="list" value="2080365734">
+                       <input data-id="source:input" type="hidden" name="source" value="EFD">
+                       <input data-id="required:input" type="hidden" name="required" value="list,email">
+                       <input data-id="url:input" type="hidden" name="url" value="">   
+                       <p data-id="Email Address:p" ></p>
+                           <label data-id="Email Address:label" data-name="email" class="ctct-form-required">
+                               <input data-id="Email Address:input" type="text" name="email" value="" maxlength="80" placeholder="Email">
+                           </label>
+                       <button type="submit" class="suscribe-button Button ctct-button Button--block Button-secondary" data-enabled="enabled">Subscribe</button>
+                   </form>
+               </div>
+            <!--End CTCT Sign-Up Form-->
+            
 	   <div class="mobile-only">
 			 <nav class="component--social-menu">
 			   <a class="menuitem" href="https://www.facebook.com/ecocitybuilders/">
@@ -71,12 +77,22 @@
 	 </footer>
 
 </div><!-- .main-wrapper  -->
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
-<script type='text/javascript'>(function($) {window.fnames = new Array();
-    window.ftypes = new Array();fnames[0]='EMAIL';
-    ftypes[0]='email';fnames[1]='FNAME';
-    ftypes[1]='text';fnames[2]='LNAME';
-    ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
-</script>
+  <script type='text/javascript'>
+               var localizedErrMap = {};
+               localizedErrMap['required'] = 		'This field is required.';
+               localizedErrMap['ca'] = 			'An unexpected error occurred while attempting to send email.';
+               localizedErrMap['email'] = 			'Please enter your email address in name@email.com format.';
+               localizedErrMap['birthday'] = 		'Please enter birthday in MM/DD format.';
+               localizedErrMap['anniversary'] = 	'Please enter anniversary in MM/DD/YYYY format.';
+               localizedErrMap['custom_date'] = 	'Please enter this date in MM/DD/YYYY format.';
+               localizedErrMap['list'] = 			'Please select at least one email list.';
+               localizedErrMap['generic'] = 		'This field is invalid.';
+               localizedErrMap['shared'] = 		'Sorry, we could not complete your sign-up. Please contact us to resolve this.';
+               localizedErrMap['state_mismatch'] = 'Mismatched State/Province and Country.';
+                    localizedErrMap['state_province'] = 'Select a state/province';
+               localizedErrMap['selectcountry'] = 	'Select a country';
+               var postURL = 'https://visitor2.constantcontact.com/api/signup';
+  </script>
+  <script type='text/javascript' src='https://static.ctctcdn.com/h/contacts-embedded-signup-assets/1.0.2/js/signup-form.js'></script>
 </body>
 </html>

f410439e8700d621a36c1980378d51307e231f90
nicolas.martello <martellonicolas@gmail.com>
2016-09-27 11:45:32 -0300

    adding templates parts

---
+++ wp-content/themes/twentysixteen/template-parts/jumbo.php
@@ -0,0 +1,21 @@
+<div class="component--jumbo">
+  <div class="image-container active">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/bio-geo-hero.jpg">
+    <h3>Reshaping cities for the long-term health of human and natural systems</h3>
+  </div>
+
+  <div class="image-container">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/ecological-hero.jpg">
+    <h3>asd</h3>
+  </div>
+
+  <div class="image-container">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/socio-cultural-hero.jpg">
+    <h3>Biologic things</h3>
+  </div>
+
+  <div class="image-container">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/ecological-hero.jpg">
+    <h3>Biologic things</h3>
+  </div>
+</div>
---
+++ wp-content/themes/twentysixteen/template-parts/social_networks.php
@@ -0,0 +1,11 @@
+<nav class="component--social-menu">
+  <a class="menuitem" href="https://www.facebook.com/ecocitybuilders/" target="_blank">
+    <span class="ms-icon icon-facebook-white-socialico"></span>
+  </a>
+  <a class="menuitem" href="https://twitter.com/ecocitybuilder" target="_blank">
+    <span class="ms-icon icon-twitter-white-socialico"></span>
+  </a>
+  <a class="menuitem" href="https://www.instagram.com/ecocitybuilders/" target="_blank">
+    <span class="ms-icon icon-linkedin-white-socialico"></span>
+  </a>
+</nav>

03c15ccb4e48dbcec557fa7added2ee3e26529e4
Martin Sacco <msacco@Martins-MacBook-Pro.local>
2016-10-03 18:54:27 -0300

    blog page integrated

--- wp-content/themes/twentysixteen/ajax-pagination.php
+++ wp-content/themes/twentysixteen/ajax-pagination.php
@@ -1,53 +1,55 @@
 <?php
-session_start(); //set session to remember the categories while traversing through the paginated results
+  session_start(); //set session to remember the categories while traversing through the paginated results
 
 
-//continue only if $_POST is set and it is a Ajax request
-if(isset($_POST) && isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
+  //continue only if $_POST is set and it is a Ajax request
+  if(isset($_POST) && isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
 
-require_once('../../../wp-load.php');
-global $wpdb;
+  require_once('../../../wp-load.php');
+  global $wpdb;
 
-$item_per_page = 2;
+  $item_per_page = 2;
 
+  if( !isset( $_SESSION['cats'] ) ){
+    $_SESSION['cats'] = "-1"; //Set a default category to display on page load
+  }
 
-if( !isset( $_SESSION['cats'] ) ){
-      $_SESSION['cats'] = "-1"; //Set a default category to display on page load
-}
-	if( isset( $_POST['cat']) ) {
-            $_SESSION['cats'] = $_POST['cat']; /* save it to a session so that the query remembers */
-        } 
+  if( isset( $_POST['cat']) ) {
+    $_SESSION['cats'] = $_POST['cat']; /* save it to a session so that the query remembers */
+  } 
 
-        if( !isset( $_SESSION['author'] ) ){
-                $_SESSION['author'] = "-1"; //Set a default category to display on page load
-        }
-	if( isset( $_POST['auth']) ) {
-            $_SESSION['author'] = $_POST['auth']; /* save it to a session so that the query remembers */
-        } 
-        if( !isset( $_SESSION['date'] ) ){
-                $_SESSION['date'] = "DESC"; //Set a default category to display on page load
-        }
-	if( isset( $_POST['dat']) ) {
-            $_SESSION['date'] = $_POST['dat']; /* save it to a session so that the query remembers */
-        } 
-	//Get page number from Ajax POST
-	if(isset($_POST["page"])) {
+  if( !isset( $_SESSION['author'] ) ){
+    $_SESSION['author'] = "-1"; //Set a default category to display on page load
+  }
 
-		$page_number = filter_var($_POST["page"], FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_STRIP_HIGH); //filter number
-		
-        if( !is_numeric($page_number)) {
+  if( isset( $_POST['auth']) ) {
+    $_SESSION['author'] = $_POST['auth']; /* save it to a session so that the query remembers */
+  } 
 
-            die('Invalid page number!'); //incase of invalid page number
+  if( !isset( $_SESSION['date'] ) ){
+    $_SESSION['date'] = "DESC"; //Set a default category to display on page load
+  }
 
-        } 
+  if( isset( $_POST['dat']) ) {
+    $_SESSION['date'] = $_POST['dat']; /* save it to a session so that the query remembers */
+  } 
 
-	} else{
+  //Get page number from Ajax POST
+  if(isset($_POST["page"])) {
 
-		$page_number = 1; //if there's no page number, set it to 1
+    $page_number = filter_var($_POST["page"], FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_STRIP_HIGH); //filter number
+    
+      if( !is_numeric($page_number)) {
+        die('Invalid page number!'); //incase of invalid page number
+      } 
 
-	}
-        var_dump($_SESSION['date']);
-$results = $wpdb->get_var("
+  } else {
+    $page_number = 1; //if there's no page number, set it to 1
+  }
+  
+  var_dump($_SESSION['date']);
+
+  $results = $wpdb->get_var("
     SELECT COUNT(*) FROM {$wpdb->posts} INNER JOIN {$wpdb->term_relationships} ON ({$wpdb->posts}.ID = {$wpdb->term_relationships}.object_id)
     INNER JOIN {$wpdb->term_taxonomy} ON ({$wpdb->term_relationships}.term_taxonomy_id = {$wpdb->term_taxonomy}.term_taxonomy_id)
     WHERE ((". $_SESSION['cats']." = '-1' OR {$wpdb->term_taxonomy}.term_id = ". $_SESSION['cats'].")
@@ -55,160 +57,97 @@ $results = $wpdb->get_var("
     AND {$wpdb->term_taxonomy}.taxonomy = 'category'
     AND {$wpdb->posts}.post_type = 'post'
     AND {$wpdb->posts}.post_status = 'publish')"
-);
+  );
 
-	$get_total_rows = $results; //hold  number of rows in a variable
+  $get_total_rows = $results; //hold  number of rows in a variable
 
-	$total_pages = ceil( $get_total_rows[0]/$item_per_page );
-	//get starting position to fetch the records
-	$page_position = ( ( $page_number-1 ) * $item_per_page );
-	
+  $total_pages = ceil( $get_total_rows[0]/$item_per_page );
+  //get starting position to fetch the records
+  $page_position = ( ( $page_number-1 ) * $item_per_page );
+  
 
-$sql = "
-        SELECT  {$wpdb->posts}.* FROM {$wpdb->posts}
-        INNER JOIN {$wpdb->term_relationships} ON ({$wpdb->posts}.ID = {$wpdb->term_relationships}.object_id)
-        INNER JOIN {$wpdb->term_taxonomy} ON ({$wpdb->term_relationships}.term_taxonomy_id = {$wpdb->term_taxonomy}.term_taxonomy_id)
-        WHERE ((". $_SESSION['cats']." = '-1' OR {$wpdb->term_taxonomy}.term_id IN(". $_SESSION['cats']."))
-            AND (". $_SESSION['author']." = '-1' OR {$wpdb->posts}.post_author = ". $_SESSION['author'].")
-        AND {$wpdb->term_taxonomy}.taxonomy = 'category'
-        AND {$wpdb->posts}.post_type = 'post'
-        AND {$wpdb->posts}.post_status = 'publish')
-        ORDER BY {$wpdb->posts}.post_date  ". $_SESSION['date']." LIMIT ".$page_position.", ".$item_per_page."; 
-     ";
-	
-	//Prepare our MySQL results so that we can output each and every blog post related to the current categories
-$getall = $wpdb->get_col( $sql );
+  $sql = "
+    SELECT  {$wpdb->posts}.* FROM {$wpdb->posts}
+    INNER JOIN {$wpdb->term_relationships} ON ({$wpdb->posts}.ID = {$wpdb->term_relationships}.object_id)
+    INNER JOIN {$wpdb->term_taxonomy} ON ({$wpdb->term_relationships}.term_taxonomy_id = {$wpdb->term_taxonomy}.term_taxonomy_id)
+    WHERE ((". $_SESSION['cats']." = '-1' OR {$wpdb->term_taxonomy}.term_id IN(". $_SESSION['cats']."))
+        AND (". $_SESSION['author']." = '-1' OR {$wpdb->posts}.post_author = ". $_SESSION['author'].")
+    AND {$wpdb->term_taxonomy}.taxonomy = 'category'
+    AND {$wpdb->posts}.post_type = 'post'
+    AND {$wpdb->posts}.post_status = 'publish')
+    ORDER BY {$wpdb->posts}.post_date  ". $_SESSION['date']." LIMIT ".$page_position.", ".$item_per_page."; 
+  ";
 
-if ( $getall ) 
-{
-    foreach ( $getall as $id ) 
-    { 
-        $post = get_post( intval( $id ) );
-        setup_postdata( $post );
+  //Prepare our MySQL results so that we can output each and every blog post related to the current categories
+  $getall = $wpdb->get_col( $sql );
+
+  if ( $getall ) {
+    foreach ( $getall as $id ) { 
+      $post = get_post( intval( $id ) );
+      setup_postdata( $post );
+
+      //category
+      $categoryName = "";
+      foreach(get_the_category() as $category) {
+        $categoryName .= ' ' . $category->cat_name;
+      }
+
+      //content
+      $content = substr(get_the_content(), 0, 209);
 ?>
 
+<article class="article blog">
+  <div class="wrapper" href="#">
+    <span class="text">
+      <h2><?php echo $categoryName; ?></h2>
+      <h3><?php the_title(); ?></h3>
+      <h4><?php the_time('M d Y'); ?></h4>
+      <span class="desktop-only"><?php echo get_the_content(); ?></span>
+    </span>
+  </div>
+</article>
 
-
-
-
-<!--Begin output of blog posts-->
-               <div class="blog-post" >
-                <div class="header"> 
-                    <?php
-                    $categories = get_the_category(); 
-                      foreach($categories as $category) {
-                           echo "<span>" . $category->cat_name . "</span>";
-                    }
-                    ?>
-                </div>
-                <h1><?php the_title(); ?></h1>
-                    <div class="article">
-                        <div class="lhs-column">
-                    <?php
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail('ajax-pagination');
-                        }
-                    ?>
-                        </div>
-                        <div class="rhs-column">
-
-                            <div class="meta-row-one">
-                                <div class="author">
-                                    <b><?php the_time('M d Y'); ?></b> 
-                                    <b>By</b> <?php the_author_posts_link(); ?>
-                                </div>
-                                <div style="clear:both;"></div>
-                            </div>
-                    
-                            <div class="meta-row-two">
-                            <?php
-
-                            $content = get_the_content();
-                            echo substr($content, 0, 209);  
-
-                             ?>
-                            </div>
-                            <div class="meta-row-three">
-                            <a href=""></a>
-                            </div>
-                    </div>
-                    <div style="clear:both;"></div>
-                </div><!--End of container -->
-                </div>
-
-<div class="divider">&nbsp;</div>
-
-<!--End output of blog posts-->
-        <?php
+<?php
+      } 
     } 
-}	
 
-	echo '<div align="center">';
-
-	echo paginate_function( $item_per_page, $page_number, $get_total_rows[0], $total_pages ); /* The pagination func to display the links etc */
-
-	echo '</div>';
-}
+    echo paginate_function( $item_per_page, $page_number, $get_total_rows[0], $total_pages ); /* The pagination func to display the links etc */
+  }
 
 
-################ pagination function #########################################
-function paginate_function($item_per_page, $current_page, $total_records, $total_pages)
-{
+  ################ pagination function #########################################
+  function paginate_function($item_per_page, $current_page, $total_records, $total_pages) {
     $pagination = '';
 
+    if($total_pages > 0 && $total_pages != 1 && $current_page <= $total_pages) { //verify total pages and current page number
 
-    if($total_pages > 0 && $total_pages != 1 && $current_page <= $total_pages){ //verify total pages and current page number
+      $pagination .= '<div class="pagination-container">';
+      
+      $right_links = $current_page + 3; 
+      $previous    = $current_page - 3; // previous link 
+      $previ       = $current_page - 1; // Additional variable to go back only one page      
+      $next        = $current_page + 1; // next link
+      $first_link  = true; //boolean var to decide our first link
+      
+      if($current_page > 1){
+        $previous_link = ($previous == 0)? 1 : $previous;
+        //$pagination .= '<a href="#" class="prev" data-page="'.$previ.'" title="Previous">&lt; Previous</a></li>';
+        $first_link = false; //set first link to false
+      } else { // Setup a dud link if were on the first page already - Had to set this to keep the links in order
+        //$pagination .= '<li class="pagi-prev-dud">&lt; PREVIOUS</li>'; //previous link
+      }
 
-        $pagination .= '<ul class="ajax-pagination">';
-        
-        $right_links    = $current_page + 3; 
-        $previous       = $current_page - 3; //previous link 
-        $previ       =   $current_page - 1; // Additional variable to go back only one page      
-        $next           = $current_page + 1; //next link
-        $first_link     = true; //boolean var to decide our first link
-        
-        if($current_page > 1){
-			$previous_link = ($previous==0)?1:$previous;
-            $pagination .= '<li class="pagi-prev"><a href="#"  data-page="'.$previ.'" title="Previous">&lt; Previous</a></li>'; //previous link
-//                for($i = ($current_page-2); $i < $current_page; $i++){ //Create left-hand side links
-//                    if($i > 0){
-//                        $pagination .= '<li><a href="#" data-page="'.$i.'" title="Page'.$i.'">'.$i.'</a></li>';
-//                    }
-//                }   
-            $first_link = false; //set first link to false
-        } else { // Setup a dud link if were on the first page already - Had to set this to keep the links in order
-            $pagination .= '<li class="pagi-prev-dud">&lt; PREVIOUS</li>'; //previous link
-        }
-//        
-//        if($first_link){ //if current active page is first link
-//            $pagination .= '<li class="first active">'.$current_page.'</li>';
-//        }elseif($current_page == $total_pages){ //if it's the last active link
-//            $pagination .= '<li class="last active">'.$current_page.'</li>';
-//        }else{ //regular current link
-//            $pagination .= '<li class="active">'.$current_page.'</li>';
-//        }
-//            
-//
-//        for($i = $current_page+1; $i < $right_links ; $i++){ //create right-hand side links
-//            if($i<=$total_pages){
-//                $pagination .= '<li><a href="#" data-page="'.$i.'" title="Page '.$i.'">'.$i.'</a></li>';
-//            }
-//        }
-
-
-        if($current_page < $total_pages){ 
-				$next_link = ($i > $total_pages)? $total_pages : $i;
-                $pagination .= '<li class="pagi-next"><a href="#"   data-page="'.$next.'" title="Next">See More &gt;</a></li>'; //next link
-        } else { // Setup a dud link if were on the first page already - Had to set this to keep the links in order
-            $pagination .= '<li class="pagi-next-dud">NEXT &gt;</li>'; 
-        }
-        
-
-        $pagination .= '</ul>'; 
-
-
+      if($current_page < $total_pages) { 
+        $next_link = ($i > $total_pages)? $total_pages : $i;
+        $pagination .= '<a href="#" id="nextbutton" class="next" data-page="'.$next.'" title="Next">See More</a></li>'; //next link
+      } else { // Setup a dud link if were on the first page already - Had to set this to keep the links in order
+        //$pagination .= '<li class="pagi-next-dud">NEXT &gt;</li>'; 
+      }
+      
+      $pagination .= '</div>';
     }
     return $pagination; //return pagination links
-}
+  }
 
 
+?>
--- wp-content/themes/twentysixteen/js/ajax-custom.js
+++ wp-content/themes/twentysixteen/js/ajax-custom.js
@@ -1,180 +1,92 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 (function($) {
 
+  $(document).ready(function(){
+    rememberstates(); //Function remembers the checkboxes states
+    getFilteredPosts();
 
-/* When clicking ajax pagination links display loading logo while loading results */
-function AjaxPaginationButtons(){
+    $("#results").on("click", "#nextbutton", paginationHandler);
 
-    $("#results").on( "click", ".ajax-pagination a", function (e){
-
-        e.preventDefault();
-
-        $(".loading-div").show(); //show loading element
-
-        var page = $(this).attr("data-page"); //get page number from link
-
-        $("#results").load( wnm_custom.template_url + "/ajax-pagination.php",{"page":page}, function(){ //Load results from fetch_pages.php and post the page number in a variable
-
-            $(".loading-div").hide(); //once done, hide loading element
-
-        });
-        
+    $('.filter-blog').on('change', function(e) {
+      setLocalCookies(); //Function to set the cookies states
+      getFilteredPosts();
+      e.preventDefault();
     });
+  });
 
-}
+  /* When clicking ajax pagination links display loading logo while loading results */
+  function paginationHandler(e) {
+    var page = $(this).attr("data-page"); //get page number from link
+    $(this).remove();
+    getFilteredPosts(page);
+    e.preventDefault();
+  }
 
+  /*
+  Loop through all localstorage cookies and assign them to a variable, then check all the checkboxes ID's to see if 
+  they match and if they do we set the checkboxes attribute the checked so that it stays checked after page refresh
+  */
+  function rememberstates() {
+    for (var i = 0; i < localStorage.length; i++) {
+      var cookies = localStorage.key(i);
 
+      $(".checkboxcat").each(function(i, e) {
+        var getCheckboxID = $(this).attr('id');
 
-/*
-Loop through all localstorage cookies and assign them to a variable, then check all the checkboxes ID's to see if 
-they match and if they do we set the checkboxes attribute the checked so that it stays checked after page refresh
-*/
-function rememberstates(){
-
-for(var i = 0; i < localStorage.length; i++){
-    
-    var cookies = localStorage.key(i);
-
-        $(".checkboxcat").each(function(i,e){
-
-            var getCheckboxID = $(this).attr('id');
-
-            if ( getCheckboxID === cookies)
-
-               $(this).attr('checked', 'checked');
-
-           });
-}
-
-}
-
-
-
-/*
-When clicking checkboxes this function will create or destroy a cookie depending what state you 
-leave the checkbox after clicking
-*/
-function setLocalCookies(){
-
-    $(".checkboxcat").each(function(i,e){
-        if( $(this).is(":checked") )
-        {
-
-                var checkboxID = $(this).attr("id"); //set the name of the cookie equal to the dynamic ID name
-
-                var checkboxDataTag = $(this).attr("data-cat")  //set the cookie value equal to the data-cat attribute, just an integer
-                // alert(checkboxID);
-                localStorage.setItem( checkboxID, checkboxDataTag);
-
-        } else if( $(this).is(":not(:checked)") ) {
-                
-                var checkboxID = $(this).attr("id");
-                //alert(checkboxID);
-                localStorage.removeItem( checkboxID );
+        if (getCheckboxID === cookies) {
+          $(this).attr('checked', 'checked');
         }
+      });
+    }
+  }
+
+  /*
+  When clicking checkboxes this function will create or destroy a cookie depending what state you 
+  leave the checkbox after clicking
+  */
+  function setLocalCookies() {
+    $(".checkboxcat").each(function(i, e) {
+      if ($(this).is(":checked")) {
+        var checkboxID = $(this).attr("id"); //set the name of the cookie equal to the dynamic ID name
+        var checkboxDataTag = $(this).attr("data-cat") //set the cookie value equal to the data-cat attribute, just an integer
+        localStorage.setItem(checkboxID, checkboxDataTag);
+      } else if ($(this).is(":not(:checked)")) {
+        var checkboxID = $(this).attr("id");
+        localStorage.removeItem(checkboxID);
+      }
     });
-    
-}
+  }
 
+  function getFilteredPosts(page) {
+    var categoryChosen = $('#category').val();
+    var authorChosen = $('#author').val();
+    var dateChosen = $('#date-filter').val();
 
+    $.ajax({
+      cache: false,
+      timeout: 8000,
+      url: wnm_custom.template_url + "/ajax-pagination.php",
+      type: "POST",
+      data: {
+        'cat': categoryChosen,
+        'auth': authorChosen,
+        'dat': dateChosen,
+        'page': page || 1
+      },
 
-/*
-When we first load the page this will load whatever category we specified as the default to display in fetch_pages.php where you 
-can set $_SESSION['cats'] = "3";  on line 20, substituting whatever default category ID you wish
-*/
-$( window ).load(function() {
+      success: function(data, textStatus, jqXHR) {
+        if (page) {
+          $('#results').append(data).fadeIn();
+        } else {
+          $('#results').html(data).fadeIn();
+        }
+      },
 
-    rememberstates(); //Function remembers the checkboxes states
-
-            $.ajax({
-                cache: false,
-                timeout: 8000,
-                url:  wnm_custom.template_url + "/ajax-pagination.php",
-                type: "POST",
-
-                success: function( data, textStatus, jqXHR ){
-                    var $ajax_response = $( data );
-                   $("#results").append( data );
-                },
-
-                error: function( jqXHR, textStatus, errorThrown ){
-                    console.log( 'The following error occured: ' + textStatus, errorThrown );   
-                },
-
-                complete: function( jqXHR, textStatus ){
-
-                }
-            });
-
-
-
-    });
-
-
-
-
-
-/*
-This is the normal function which will be utilized whenever choosing categories and navigating through the paginated results
-*/
-$(document).ready(function(){
-
-    rememberstates(); //Function remembers the checkboxes states
-   
-    $( '.filter-blog' ).on( 'change', function( e ) { 
-
-        e.preventDefault();
-
-        setLocalCookies(); //Function to set the cookies states
-
-        var categoryChosen = $('#category').val(); 
-        var authorChosen = $('#author').val(); 
-        var dateChosen = $('#date-filter').val(); 
-
-
-           $.ajax({
-                cache: false,
-                timeout: 8000,
-                url:  wnm_custom.template_url + "/ajax-pagination.php",
-                type: "POST",
-                data: {'cat' : categoryChosen, 'auth' : authorChosen, 'dat' : dateChosen },
-
-               beforeSend: function() {                    
-                    $( '#results' ).html( '<img class="loadingSVG" src="' + wnm_custom.template_url + '/images/loading-cylon-red.svg" width="256" height="32" >' );
-                },
-
-                success: function( data, textStatus, jqXHR ){
-
-                    var $ajax_response = $( data );
-
-                    $('#results').html($ajax_response).fadeIn(); 
-                },
-
-                error: function( jqXHR, textStatus, errorThrown ){
-                    console.log( 'The following error occured: ' + textStatus, errorThrown );   
-                },
-
-                complete: function( jqXHR, textStatus ){
-
-                }
-
-            });
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log('The following error occured: ' + textStatus, errorThrown);
+      }
 
     });
-    
-AjaxPaginationButtons();
-
-
-});
-
-
+  }
 
 })(jQuery);
-
 

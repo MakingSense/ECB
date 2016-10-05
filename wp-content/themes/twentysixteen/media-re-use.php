94b424c3e2673ea94473997d313bb4c7d15f36d5
daniela_rueda <drueda@makingsense.com>
2016-10-05 12:58:36 -0300

    Se creo un nuevo template reutilizable

---
+++ wp-content/themes/twentysixteen/media-re-use.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+Template Name:  Page Media re-use
+ */
+/**
+ * The media template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+get_header(); ?>                
+    <main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'template-parts/content-page', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+
+			// End of the loop.
+		endwhile;
+		?>
+
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>  
+<?php get_footer(); ?>

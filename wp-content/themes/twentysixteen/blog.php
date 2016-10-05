cbd25c75ec46fcd9bb94a1d6b01a5d24ad360311
daniela_rueda <drueda@makingsense.com>
2016-10-05 12:57:01 -0300

    Refactorizar codigo

--- wp-content/themes/twentysixteen/blog.php
+++ wp-content/themes/twentysixteen/blog.php
@@ -1,4 +1,21 @@
 <?php
+/*
+Template Name: Page Blog
+*/
+/**
+ * The media template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
   wp_reset_query();
   $paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
   $args = array(

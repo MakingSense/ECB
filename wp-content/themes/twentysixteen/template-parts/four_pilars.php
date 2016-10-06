6bcdde9eec7a9c124502e0f3557b5be7e57b9691
nicolas martello <martellonicolas@gmail.com>
2016-10-06 08:55:52 -0300

    four pilars refactor

--- wp-content/themes/twentysixteen/template-parts/four_pilars.php
+++ wp-content/themes/twentysixteen/template-parts/four_pilars.php
@@ -1,32 +1,49 @@
 <?php
-  $pilar_text = array(
-    'urban_design' => 'The city is designed and developed with the underlying principle of access by proximity, providing residents with walkable access to open/green spaces, basic urban services, and affordable housing. It demonstrates environmentally friendly transport options and provides walking and transit access to close-by employment.',
-    'bio_geo' => 'The city commits to responsible management of resources and materials as well as the generation and use of clean, renewable energy. It maintains a level of physical conditions that ensure clean air and access to clean and safe water. It fosters healthy soil and makes sure nutritious, locally grown food is available.',
-    'socio_cultural' => '',
+function get_pilars() {
+    $pilars_text = array (
+        'urban_design' => array(
+            'title' => 'Urban Design',
+            'text'  =>'The city is designed and developed with the underlying principle of access by proximity, providing residents with walkable access to open/green spaces, basic urban services, and affordable housing. It demonstrates environmentally friendly transport options and provides walking and transit access to close-by employment.'
+        ),
+        'bio_geo' => array (
+            'title' => 'Bio Geo',
+            'text'  => 'The city commits to responsible management of resources and materials as well as the generation and use of clean, renewable energy. It maintains a level of physical conditions that ensure clean air and access to clean and safe water. It fosters healthy soil and makes sure nutritious, locally grown food is available.'
+        ),
+        'socio_cultural' => array (
+            'title' => 'Socio Cultural',
+            'text'  => 'The city access to lifelong education for all and facilitates conditions for vibrant human expression, knowledge, interaction and governance by promoting cultural activities and full comunity participation. It invests in an equitable economy that benefits people and planet and is committed to the wellbeing of every citizen, regardless to socioeconomic status.',
+        ),
+        'ecological' => array (
+            'title' => 'Ecological',
+            'text'  => "The city is committed to sustaining and restoring biodiversity of local, regional and global ecosystems, including species diversity, ecosystem diversity, and genetic diversity. It keeps its demand on ecosystems within the limits of the Earth's carrying capactiy and supports ecological integrity by maintaining essential linkages within and between ecological corridors."
+        )
+     );
+     $pilars_count = 0;
 
- );
+     if (empty($pilars_text)){
+       return "";
+     }
+
+     foreach ($pilars_text as &$value) {
+        $class = '';
+
+        if ($pilars_count === 0){
+          $class = 'active';
+        }
+        $return_pilars .= '<a href="#" class="'.$class.'" data-jumbo-id="'.$pilars_count.'">'.$value['title'].'</a>'
+                         .'<p class="text active mobile-only">'.$value['text'].'</p>';
+        $pilars_count ++;
+     }
+
+     return $return_pilars;
+ }//-end function get_pilars
  ?>
 <section class="content">
 <article class="intro">
   <h2>Four Pillars of an Ecocity</h2>
   <div class="component--jumbo-selector">
     <div class="button-container">
-      <a href="#" class="active" data-jumbo-id="0">Urban Design</a>
-      <p class="text active mobile-only">
-        <?php echo $pilar_text['urban_design'] ?>
-      </p>
-      <a href="#" data-jumbo-id="1">Bio Geo</a>
-      <p class="text mobile-only">
-          <?php echo $pilar_text['bio_geo'] ?>
-      </p>
-      <a href="#" data-jumbo-id="2">Socio Cultural</a>
-      <p class="text mobile-only">
-        The city is designed and developed with the underlying principle of access by proximity, providing residents with walkable access to open/green spaces, basic urban services, and affordable housing. It demonstrates environmentally friendly transport options and provides walking and transit access to close-by employment.
-      </p>
-      <a href="#" data-jumbo-id="3">Ecological</a>
-      <p class="text mobile-only">
-        The city is designed and developed with the underlying principle of access by proximity, providing residents with walkable access to open/green spaces, basic urban services, and affordable housing. It demonstrates environmentally friendly transport options and provides walking and transit access to close-by employment.
-      </p>
+        <?php echo get_pilars(); ?>
     </div>
     <div class="text-container desktop-only">
       <p class="text active">

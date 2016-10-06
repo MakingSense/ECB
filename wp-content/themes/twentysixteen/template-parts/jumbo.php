f1f8f55147ab4ff5123ed1af5c7efd879b904837
Martin Sacco <msacco@Martins-MacBook-Pro.local>
2016-10-06 11:47:37 -0300

    footer, scripts and styles

--- wp-content/themes/twentysixteen/template-parts/jumbo.php
+++ wp-content/themes/twentysixteen/template-parts/jumbo.php
@@ -1,11 +1,11 @@
 <div class="component--jumbo">
   <div class="image-container active grayscale">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/bio-geo-hero.jpg">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/urban-design-logo.jpg">
     <h3>Reshaping cities for the long-term health of human and natural systems</h3>
   </div>
 
   <div class="image-container grayscale">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/ecological-hero.jpg">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/bio-geo-hero.jpg">
     <h3>Reshaping cities for the long-term health of human and natural systems</h3>
   </div>
 

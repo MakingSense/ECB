305448ca67fb3e57161f5d31d4cd19454d79ac3b
Martin Sacco <msacco@Martins-MacBook-Pro.local>
2016-10-13 14:45:25 -0300

    post templates fixed

--- wp-content/themes/twentysixteen/single.php
+++ wp-content/themes/twentysixteen/single.php
@@ -10,21 +10,26 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
-			// Include the single post content template.
-			if($post->post_type=="post")
-				get_template_part( 'template-parts/content', 'post' );
-			else
-				get_template_part( 'template-parts/content', 'single' );
-			// End of the loop.
-		endwhile;
-		?>
+	<main id="main" class="section--post" role="main">
+		<div class="hr-wrapper"><hr class="slim"></div>
+		<section class="content">
+			<?php
+			// Start the loop.
+			while ( have_posts() ) : the_post();
+				// Include the single post content template.
+				if($post->post_type=="post")
+					get_template_part( 'template-parts/content', 'post' );
+				else
+					get_template_part( 'template-parts/content', 'single' );
+				// End of the loop.
+			endwhile;
+			?>
+		</section>
 	</main><!-- .site-main -->
 
 	<?php get_sidebar( 'content-bottom' ); ?>
+
+	<div style="clear: both;"></div>
 
 </div><!-- .content-area -->
 <?php get_footer(); ?>

2eb44fd74064eb1b44cc93dc7cc52a53831db64b
Martin Sacco <msacco@Martins-MacBook-Pro.local>
2016-10-13 20:56:15 -0300

    Home: block categories and sizes

--- wp-content/themes/twentysixteen/template-parts/home/block-blog.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-blog.php
@@ -1,7 +1,7 @@
-<article id="post-<?php the_ID(); ?>" class="blog">
+<article id="post-<?php the_ID(); ?> - <?=the_ID(); ?>" class="blog">
   <div class="wrapper">
     <div class="text">
-      <span><?php search_category_post($post->ID) ?></span>
+      <span><?= search_category_post($post->ID) ?></span>
       <?php the_title( sprintf( '<h3>', esc_url( get_permalink() ) ), '</h3>' ); ?>
       <h4><?php the_date(); ?></h4>
       <?php

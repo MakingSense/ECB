bb309a7254780f5ba2e2ccb9b50b651637ab22a5
Martin Sacco <msacco@Martins-MacBook-Pro.local>
2016-10-13 14:50:30 -0300

    article subbar

--- wp-content/themes/twentysixteen/article.php
+++ wp-content/themes/twentysixteen/article.php
@@ -59,7 +59,7 @@
 ?>  
 
 <main role="main" class="section--article">
-  <hr class="slim">
+  <div class="hr-wrapper"><hr class="slim"></div>
   <section class="content">
 
     <div class="wrapper">

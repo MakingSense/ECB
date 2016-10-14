cc3a430f69b137836ce5bf7410314dc3fa892e66
Martin Sacco <msacco@Martins-MacBook-Pro.local>
2016-10-13 22:37:54 -0300

    BUG114

--- wp-content/themes/twentysixteen/template-parts/home/block-social.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-social.php
@@ -8,8 +8,13 @@
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));</script>
   <h2>Twitter and Facebook</h2>
-  <div class="tweet-container">
+  <div class="tweet-container mobile-only">
     <div class="tweet"><div class="fb-page" data-href="https://www.facebook.com/ecocitybuilders" data-tabs="timeline" data-width="307" data-height="337" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"><blockquote cite="https://www.facebook.com/ecocitybuilders" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ecocitybuilders"></a></blockquote></div></div>
     <div class="tweet"><a class="twitter-timeline" data-width="307" data-height="337" href="https://twitter.com/TwitterDev"></a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+  </div>
+
+  <div class="tweet-container desktop-only">
+    <div class="tweet"><div class="fb-page" data-href="https://www.facebook.com/ecocitybuilders" data-tabs="timeline" data-width="427" data-height="466" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"><blockquote cite="https://www.facebook.com/ecocitybuilders" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ecocitybuilders"></a></blockquote></div></div>
+    <div class="tweet"><a class="twitter-timeline" data-width="427" data-height="466" href="https://twitter.com/TwitterDev"></a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></div>
   </div>
 </article>

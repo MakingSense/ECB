342dcec6117b7dece2507ffd04ee8f5f9a6bc51a
Martin Sacco <msacco@Martins-MacBook-Pro.local>
2016-10-14 12:54:37 -0300

    BUG81

--- wp-content/themes/twentysixteen/header.php
+++ wp-content/themes/twentysixteen/header.php
@@ -15,7 +15,7 @@ $primary_menu = get_primary_menu();
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">

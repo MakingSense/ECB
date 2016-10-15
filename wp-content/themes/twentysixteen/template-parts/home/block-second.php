d632740f9558ab743c2f8d581b03d7094299d963
Damián Muti <damianmuti@gmail.com>
2016-10-15 15:37:59 -0300

    Fixed issues with dynamic image

--- wp-content/themes/twentysixteen/template-parts/home/block-second.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-second.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" class="block-second">
   <h2>Urbinsight</h2>
   <div class="wrapper">
-    <div class="text" style="background: url(<?php echo get_field('image_feature')); ?>; background-size: cover">
+    <div class="text" style="background: url(<?php echo get_field('image_feature'); ?>); background-size: cover">
       <?php the_title( sprintf( '<h3>', esc_url( get_permalink() ) ), '</h3>' ); ?>
       <h4><?php the_date(); ?></h4>
       <p><?php custom_excerpt_length(the_excerpt()); ?></p>

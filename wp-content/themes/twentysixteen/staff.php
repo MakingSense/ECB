21efb2d761a4051d6a160135400d133df7a0bd11
msaccoms <msacco@Martins-MacBook-Pro.local>
2016-10-17 15:42:41 -0300

    Staff and Board section and font variables

--- wp-content/themes/twentysixteen/staff.php
+++ wp-content/themes/twentysixteen/staff.php
@@ -5,36 +5,53 @@ Template Name: Page Staff
 */
   get_header();
   wp_reset_postdata();
-  $staff_posts=get_field('staff_posts',get_the_ID());
-  
-?>  
 
-<main role="main" class="section--article">
+  class Staff {
+    public function __construct(){
+      $this->title = get_the_title();
+      $this->members = $this->loadMembers();
+    }
+
+    private function loadMembers() {
+      $members = [];
+      $rawMembers = get_field('staff_posts', get_the_ID());
+      foreach ($rawMembers as $rawMember) {
+        array_push($members, new StaffMember($rawMember));
+      }
+      return $members;
+    }
+  }
+
+  class StaffMember {
+    public function __construct($raw){
+      $this->name = $raw->post_title;
+      $this->position = get_field('position', $raw->ID);
+      $this->image = get_field('image', $raw->ID);
+      $this->description = str_replace(']]>', ']]&gt;', apply_filters('the_content', $raw->post_content));
+    }
+  }
+
+  $staff = new Staff;
+?>
+
+<main role="main" class="section--staff">
+  <div class="head">
+    <h1><?= $staff->title ?></h1>
+  </div>
   <section class="content">
-
-    <?php foreach($staff_posts as $post) { ?>
-    	<fieldset>
-    		<legend>Member</legend>
-    		<p>Nombre: <?=get_the_title()?></p>
-    		<img src="<?=get_field('image',$post->ID)?>" />
-    		<p>Position: <?=get_field('position',$post->ID)?></p>
-    		<p><?php
-    			$content = apply_filters('the_content', $post->post_content);
-  				$content = str_replace(']]>', ']]&gt;', $content);
-  				echo $content;	
-    			?>
-    		</p>
-    	</fieldset>
-    	<br />
-
-    <?php } ?>
-      
-
+    <?php foreach($staff->members as $member) : ?>
+      <article class="member">
+        <div class="image"><figure><img src="<?= $member->image ?>" /></figure></div>
+        <div class="text">
+          <h2><?= $member->name ?></h2>
+          <h3><?= $member->position ?></h3>
+          <p><?= $member->description ?></p>
+        </div>
+      </article>
+    <?php endforeach; ?>
   </section>
-
 </main>
 
 <?php
-  get_footer(); 
+  get_footer();
 ?>
-

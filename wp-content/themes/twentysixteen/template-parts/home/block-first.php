5ca83315650041fb5a577e05259504fc53b931ff
Federico Cardoso <hernan.f.cardoso@gmail.com>
2016-10-19 15:30:03 -0300

    date optional in block post home

--- wp-content/themes/twentysixteen/template-parts/home/block-first.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-first.php
@@ -4,7 +4,9 @@
     <?php twentysixteen_post_thumbnail(); ?>
     <div class="text">
       <?php the_title( sprintf( '<h3>', esc_url( get_permalink() ) ), '</h3>' ); ?>
-      <h4><?php the_date(); ?></h4>
+      <?php if(trim(get_field("date", get_the_ID() ))!="") {?>
+      <h4><?=get_field("date", get_the_ID() ) ?></h4>
+      <?php } ?>
       <p><?php custom_excerpt_length(the_excerpt()); ?></p>
       <?php
           echo sprintf(

e2557a5e3d6d52ba423b2e26b1806adb4c66bada
daniela_rueda <drueda@makingsense.com>
2016-10-20 16:36:00 -0300

    Se agrego variable para feature image

--- wp-content/themes/twentysixteen/template-parts/home/block-third.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-third.php
@@ -1,6 +1,7 @@
 <article id="post-<?php the_ID(); ?>" class="block block-third">
   <h2>Featured Project</h2>
   <div class="wrapper">
+    <div class="" style="background: url(<?php echo get_field('feature_image'); ?>);"></div>
     <div class="text">
       <?php the_title( sprintf( '<h3>', esc_url( get_permalink() ) ), '</h3>' ); ?>
       <?php get_date_block_home() ?>  
@@ -9,6 +10,6 @@
           get_text_link_button_home();
       ?>
     </div>
-    <?php twentysixteen_post_thumbnail(); ?>
+    <?php // twentysixteen_post_thumbnail(); ?>
   </div>
 </article>

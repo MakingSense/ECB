6e78c25d424abc473efb4ec231676f53eccd2409
ggmanuel <mgarcia@makinsense.com>
2016-10-20 17:59:55 -0300

    Fix img thumbnails for posts in homepage

--- wp-content/themes/twentysixteen/functions.php
+++ wp-content/themes/twentysixteen/functions.php
@@ -32,10 +32,10 @@ function get_text_link_button_home() {
         $link_button = get_field('link_button');
         $button = get_field('button_name');
         echo sprintf(
-            __( '<div><a class="button-container" href="'.$link_button.'"><button class="more-button">'.$button.'</button></a></div>', 'twentysixteen' ),
+            __( '<a class="button-container" href="'.$link_button.'"><button class="more-button">'.$button.'</button></a>', 'twentysixteen' ),
             get_the_title()
         );
-    }	
+    }
 }
 function get_date_block_home() {
     if(trim(get_field("date", get_the_ID() ))!="") {

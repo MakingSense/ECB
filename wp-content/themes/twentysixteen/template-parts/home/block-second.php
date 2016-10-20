32ddda642510feaea6fae2e74f288a2b5c310c5a
daniela_rueda <drueda@makingsense.com>
2016-10-20 14:35:29 -0300

    correcion al llamar a funcion

--- wp-content/themes/twentysixteen/template-parts/home/block-second.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-second.php
@@ -3,10 +3,10 @@
   <div class="wrapper">
     <div class="text" style="background: url(<?php echo get_field('image_feature'); ?>); background-size: cover; -webkit-filter: grayscale(100%); filter: grayscale(100%);">
       <?php the_title( sprintf( '<h3>', esc_url( get_permalink() ) ), '</h3>' ); ?>
-      <?php date_block_home() ?>
+      <?php get_date_block_home() ?>
       <p><?php custom_excerpt_length(the_excerpt()); ?></p>
       <?php
-          buttom_block_home();
+          get_text_link_button_home();
       ?>
     </div>
   </div>

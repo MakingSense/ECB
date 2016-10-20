0aca91b3a8ddd56a0092ee97c168b3a85089456e
msaccoms <msacco@Martins-MacBook-Pro.local>
2016-10-20 12:40:43 -0300

    FE-20: Media active menu item

--- wp-content/themes/twentysixteen/ecb-ui/assets/templates/media/media.js
+++ wp-content/themes/twentysixteen/ecb-ui/assets/templates/media/media.js
@@ -1,13 +1,21 @@
 (function(){
   var section = $('.section--media');
   setCarousels();
+  setAsideMenu();
+
+  function setAsideMenu() {
+    var menus = section.find('.media-menu');
+    menus.find('.menuitem').on('click', function(e){
+      menus.removeClass('active').eq($(this).index()).addClass('active');
+    });
+  }
 
   function setCarousels() {
     section.find('.stats.mobile-only.owl-carousel').owlCarousel(getCarouselConfig({
       items: 1,
       nav: true
     }));
-    
+
     section.find('.video-collection.mobile-only.owl-carousel').owlCarousel(getCarouselConfig({
       items: 1,
       dots: true,

d1612eeb4aabb4ab127a5511c3c020991a7d3417
daniela_rueda <drueda@makingsense.com>
2016-10-20 16:35:53 -0300

    Se agrego variable para feature image

--- wp-content/themes/twentysixteen/template-parts/home/block-second.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-second.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" class="block block-second">
   <h2>Urbinsight</h2>
   <div class="wrapper">
-    <div class="background-img" style="background: url(<?php echo get_field('image_feature'); ?>); background-size: cover; -webkit-filter: grayscale(1); filter: grayscale(1);"></div>
+    <div class="background-img" style="background: url(<?php echo get_field('feature_image'); ?>); background-size: cover; -webkit-filter: grayscale(1); filter: grayscale(1);"></div>
     <div class="text">
       <?php the_title( sprintf( '<h3>', esc_url( get_permalink() ) ), '</h3>' ); ?>
       <?php get_date_block_home() ?>

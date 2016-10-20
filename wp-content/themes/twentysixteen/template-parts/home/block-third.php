20060e40ae5c3919dbaa6549fa886ba529b9bbc5
daniela_rueda <drueda@makingsense.com>
2016-10-20 11:03:23 -0300

    Refactorizar codigo

--- wp-content/themes/twentysixteen/template-parts/home/block-third.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-third.php
@@ -3,17 +3,10 @@
   <div class="wrapper">
     <div class="text">
       <?php the_title( sprintf( '<h3>', esc_url( get_permalink() ) ), '</h3>' ); ?>
-      <?php if(trim(get_field("date", get_the_ID() ))!="") {?>
-        <h4><?=get_field("date", get_the_ID() ) ?></h4>
-      <?php } ?>  
+      <?php date_block_home() ?>  
       <p><?php custom_excerpt_length(the_excerpt()); ?></p>
       <?php
-          $link_button = get_field('link_button');
-          $button = get_field('button_name');
-          echo sprintf(
-            __( '<div><a class="button-container" href="'.$link_button.'"><button class="more-button">'.$button.'</button></a></div>', 'twentysixteen' ),
-            get_the_title()
-          );
+          buttom_block_home();
       ?>
     </div>
     <?php twentysixteen_post_thumbnail(); ?>

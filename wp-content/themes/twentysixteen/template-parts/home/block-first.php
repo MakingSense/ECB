4ff21eb7127fe43d1fca4277232dfa48b89c9b2f
daniela_rueda <drueda@makingsense.com>
2016-10-20 16:35:46 -0300

    Se agrego variable para feature image

--- wp-content/themes/twentysixteen/template-parts/home/block-first.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-first.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" class="block block-first">
   <h2>Ecocity World Summit</h2>
   <div class="wrapper">
-    <?php twentysixteen_post_thumbnail(); ?>
+    <div class="" style="background: url(<?php echo get_field('feature_image'); ?>);"></div>
     <div class="text">
       <?php the_title( sprintf( '<h3>', esc_url( get_permalink() ) ), '</h3>' ); ?>
       <?php get_date_block_home() ?>

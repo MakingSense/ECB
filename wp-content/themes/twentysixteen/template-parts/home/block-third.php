b2d12fb65ba9679a26c9dedaa900f638377b9690
nicolas.martello <martellonicolas@gmail.com>
2016-10-21 18:17:06 -0300

    title length restricion to 50 charactes - text length restriction custom by block order

--- wp-content/themes/twentysixteen/template-parts/home/block-third.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-third.php
@@ -1,12 +1,14 @@
+
+
 <article id="post-<?php the_ID(); ?>" class="block block-third">
   <h2>Featured Project</h2>
   <div class="wrapper">
     <div class="text">
-      <?php the_title( sprintf( '<h3>', esc_url( get_permalink() ) ), '</h3>' ); ?>
-      <?php get_date_block_home() ?>
-      <?php custom_excerpt_length(the_excerpt()); ?>
       <?php
-          get_text_link_button_home();
+        echo the_title('<h3>','</h3>');
+       get_date_block_home();
+       echo mb_strimwidth(strip_tags(get_the_content()), 0, 258, "..." );
+       get_text_link_button_home();
       ?>
     </div>
     <a class="post-thumbnail" href="<?php echo get_field('link_button'); ?>"><img src="<?php echo get_field('feature_image'); ?>"></a>

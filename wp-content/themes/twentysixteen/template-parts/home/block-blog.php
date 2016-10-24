eaf7888403cd4d1cf0a1de7511a95096d757b97a
ggmanuel <mgarcia@makinsense.com>
2016-10-24 18:01:25 -0300

    Blogs background images

--- wp-content/themes/twentysixteen/template-parts/home/block-blog.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-blog.php
@@ -1,6 +1,13 @@
 <article id="post-<?php the_ID(); ?> - <?=the_ID(); ?>" class="blog">
   <div class="wrapper">
     <div class="text">
+      <?php if(get_field('feature_image')) {
+        $style = "background: url(".get_field('feature_image').")";
+      } else {
+        $style = "background-color: white";
+      }
+      ?>
+      <div class="background-img"  style="<?= $style ?>"></div>
       <span><?= search_category_post($post->ID) ?></span>
       <?php the_title( sprintf( '<h3>', esc_url( get_permalink() ) ), '</h3>' ); ?>
       <h4><?= get_the_date() . ' | '; ?><?= get_author_name() ?></h4>

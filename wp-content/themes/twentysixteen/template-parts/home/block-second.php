ab31e39f3ebae559e8fb53e89dcf4d0178f1e2d5
ggmanuel <mgarcia@makinsense.com>
2016-10-26 10:31:12 -0300

    Black & white image on second post (Edge)

--- wp-content/themes/twentysixteen/template-parts/home/block-second.php
+++ wp-content/themes/twentysixteen/template-parts/home/block-second.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" class="block block-second">
   <h2>Urbinsight</h2>
   <div class="wrapper">
-    <div class="background-img" style="background: url(<?php echo get_field('feature_image'); ?>); background-size: cover; -webkit-filter: grayscale(1); filter: grayscale(1);"></div>
+    <img class="background-img" src="<?php echo get_field('feature_image'); ?>">
     <div class="text">
       <h3><?=mb_strimwidth( get_the_title(),0, 53, '...' )?></h3>
       <?php

9f1e58c966aa3276963df5d25c8d4d13494e4ab8
Federico Cardoso <hernan.f.cardoso@gmail.com>
2016-10-27 13:51:25 -0300

    add articulos y post relacionados

--- wp-content/themes/twentysixteen/blog.php
+++ wp-content/themes/twentysixteen/blog.php
@@ -35,7 +35,80 @@ Template Name: Page Blog
 
   <section class="content">
 
-    <?php get_navigation(); ?>
+<?php if( get_field('featured_articles',get_the_ID())) { ?>
+  <?php $mobile=array();?>
+    <section>
+      <h2>Featured</h2>
+      <section class="component--featured">
+      	<div class="article-wrapper">
+    		<section class="article-container desktop-only">
+             <?php foreach( get_field('featured_articles',get_the_ID()) as $featured) { ?>
+              <article class="article featured">
+    						<div class="wrapper">
+    							<a class="text" style="background-image: url();" href="<?=get_permalink($featured->ID)?>">
+                    <?php
+                    $cat="Uncategorized";
+                    $date="";
+
+                    if(get_post_type($featured->ID)=='post'){
+                      $date=get_field('date',$featured->ID);
+                      $cats=get_the_category($featured->ID);
+                      if(count($cats) > 0) {
+                        $cat=$cats[0]->name;
+                      }
+                    }
+                    else{
+                       if(get_post_type($featured->ID)=='page'){
+                         if(get_field('category', $featured->ID)) {
+                           $cat=get_field('category', $featured->ID);
+                         }
+                       }
+                    }
+                    ?>
+                    <?php if($cat!=="Uncategorized") { ?>
+                    <h2><?=$cat?></h2>
+                    <?php } ?>
+    								<h3><?=get_the_title($featured->ID)?></h3>
+                    <?php if(trim($date)=="") {
+                       $date=get_the_date('F d, Y', $featured->ID);
+                    }?>
+                    <h4><?=$date?></h4>
+
+    							</a>
+    						</div>
+    					</article>
+              <?php
+              $mobile[]=array('cat'=>$cat,'url'=>get_permalink($featured->ID),'title'=>get_the_title($featured->ID),'date'=>$date);
+            } ?>
+    				</section>
+          </div>
+          <section class="article-container mobile-only owl-carousel owl-loaded owl-drag">
+    					<div class="owl-stage-outer">
+                <div class="owl-stage" style="transform: translate3d(0px, 0px, 0px); transition: 0s;">
+                  <?php foreach($mobile as $item) { ?>
+                  <div class="owl-item">
+                    <article class="article featured">
+      						    <div class="wrapper">
+      							    <a class="text" style="background-image: url();" href="<?=$item['url']?>">
+                          <?php if($cat!=="Uncategorized") { ?>
+                          <h2><?=$item['cat']?></h2>
+                          <?php } ?>
+      								    <h3><?=$item['title']?></h3>
+      								    <h4><?=$date?></h4>
+      							     </a>
+      						    </div>
+      					    </article>
+                  </div>
+                <?php } ?>  
+                </div>
+              </div>
+    	       </section>
+           </section>
+
+    <?php } ?>
+
+
+
 
     <h1><?php _e('All articles'); ?></h1>
     <form  id="filter-form">
